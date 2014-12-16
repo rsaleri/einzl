98687f03346b5ddda5eac8a529e8882511a989b3
Sumit <sk@outlook.com>
2014-12-16 17:07:46 +0100

    css restructuring

--- src/js/helper.js
+++ src/js/helper.js
@@ -49,7 +49,7 @@ function notifyUser(msg, type) {
 
 // temporarily adds a class to an element and removes it after a timeout
 jQuery.fn.tempElementClass = function(c, t) {
-    var o = $(this[0]) // It's your element
+    var o = $(this[0]); // It's your element
     
     o.addClass(c);
     window.setTimeout(function() {

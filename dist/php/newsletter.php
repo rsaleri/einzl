52c856d0a17d41395dfab6588f53a6925f6abe0f
Sumit Kumar <sk@outlook.com>
2015-03-04 19:02:46 +0100

    enabled double opt-in for Newsletter sign up

--- dist/php/newsletter.php
+++ dist/php/newsletter.php
@@ -12,7 +12,7 @@ function subscribe($user, $mailchimp) {
         'id'                => 'af446b1ef2',
         'email'             => array('email'=>$user['email']),
 //        'merge_vars'        => array('FNAME'=>'Freddie', 'LNAME'=>'Smith'),
-        'double_optin'      => false,
+        'double_optin'      => true,
         'update_existing'   => true,
         'send_welcome'      => false,
     ));

4c8b7904255652da93cfa8634512d8e56103e0cc
Sumit Kumar <sk@outlook.com>
2015-06-03 10:35:46 +0200

    intendation fixes

--- src/js/models/app.js
+++ src/js/models/app.js
@@ -1,58 +1,58 @@
 var Shop = Backbone.Model.extend({
-	
-	
-	initialize: function(model) {
-		
-		var self = this;
+    
+    
+    initialize: function(model) {
+        
+        var self = this;
         
         // init some basic events (clicky stuff and all that)
         this.initEvents();
-		
-		// go get the copy
-		Einzlstck.Models.Copy = new Lang('de');
-		
-		// after we have our copy...
-		Einzlstck.Deferreds.copy.then(function(copy) {
-			
-			// ... get products from moltin
-			self.getProducts();
-			
-		});
-		
-	},
-	
-	initEvents: function() {
-		
+        
+        // go get the copy
+        Einzlstck.Models.Copy = new Lang('de');
+        
+        // after we have our copy...
+        Einzlstck.Deferreds.copy.then(function(copy) {
+            
+            // ... get products from moltin
+            self.getProducts();
+            
+        });
+        
+    },
+    
+    initEvents: function() {
+        
         // this is the cart button
-		$(document).on('vclick', '.cart-button', function(e) {
-		
-			var cartButton = $(e.currentTarget);
-			var menuButton = $('.hamburger-button');
+        $(document).on('vclick', '.cart-button', function(e) {
+        
+            var cartButton = $(e.currentTarget);
+            var menuButton = $('.hamburger-button');
 
-			if(cartButton.hasClass('open')) {
-				
-				// close cart
-				Einzlstck.Models.Cart.view.close();
+            if(cartButton.hasClass('open')) {
+                
+                // close cart
+                Einzlstck.Models.Cart.view.close();
 
-			} else {
-				
-				// open cart
-				Einzlstck.Models.Cart.view.open();
+            } else {
+                
+                // open cart
+                Einzlstck.Models.Cart.view.open();
 
-				// close menu
-				menuButton.removeClass('open');
-				$('header nav').removeClass('open');
+                // close menu
+                menuButton.removeClass('open');
+                $('header nav').removeClass('open');
 
 
-			}
+            }
 
-			e.preventDefault();
-			e.stopPropagation();
-		});
+            e.preventDefault();
+            e.stopPropagation();
+        });
         
         // this is the menu button
         $(document).on('vclick', '.hamburger-button', function(e) {
-		
+        
             var menuButton = $(e.currentTarget);
             var cartButton = $('.cart-button');
 
@@ -80,81 +80,79 @@ var Shop = Backbone.Model.extend({
             e.preventDefault();
             e.stopPropagation();
         });
-		
-	},
-	
-	getTemplate: function(path) {
-		
-		return $.get('/templates/'+path).then(function(src) {
-			
-			// is the copy already available?
-			return Einzlstck.Deferreds.copy.then(function(copy) {
-				
-				// insert copy into this html string
-				src = Einzlstck.Models.Copy.insertCopy(src, copy);
-				
-				// return a handlebars tempate with copy pre-filled
-				return Handlebars.compile(src);
-				
-			});
-			
-			
-		});
-		
-	},
-	
-	getProducts: function() {
-		
-		var self = this;
-		
-		// get product models
-		var obj = {
-			action: 'getProducts'
-		};
-		
-		return this.askServer(obj).done(function(data) {
+        
+    },
+    
+    getTemplate: function(path) {
+        
+        return $.get('/templates/'+path).then(function(src) {
+            
+            // is the copy already available?
+            return Einzlstck.Deferreds.copy.then(function(copy) {
+                
+                // insert copy into this html string
+                src = Einzlstck.Models.Copy.insertCopy(src, copy);
+                
+                // return a handlebars tempate with copy pre-filled
+                return Handlebars.compile(src);
+                
+            });
+            
+            
+        });
+        
+    },
+    
+    getProducts: function() {
+        
+        var self = this;
+        
+        // get product models
+        var obj = {
+            action: 'getProducts'
+        };
+        
+        return this.askServer(obj).done(function(data) {
 
-			if(data && data.status) {
-				
-				Einzlstck.Models.Products = [];
+            if(data && data.status) {
+                
+                Einzlstck.Models.Products = [];
 
-				$.each(data.result, function(i) {
-					Einzlstck.Models.Products[i] = new ProductModel(this);
-				});
-				
-				Einzlstck.Deferreds.products.resolve(data);
-				
-				
-			} else {
-				notifyUser(Einzlstck.Models.Copy.data.messages.noConnection, 'error');
-				console.log('GET PRODUCTS FAILED');
-				console.log(data);
-			}
-		});
-	},
-	
-	askServer: function(obj) {
-		
-		
-		return $.ajax({
-			type: 'POST',
-			dataType:'json',
-			url: 'php/ajax.php',
-			data: obj
-		}).done(function(result) {
-			
+                $.each(data.result, function(i) {
+                    Einzlstck.Models.Products[i] = new ProductModel(this);
+                });
+                
+                Einzlstck.Deferreds.products.resolve(data);
+                
+                
+            } else {
+                notifyUser(Einzlstck.Models.Copy.data.messages.noConnection, 'error');
+            }
+        });
+    },
+    
+    askServer: function(obj) {
+        
+        
+        return $.ajax({
+            type: 'POST',
+            dataType:'json',
+            url: 'php/ajax.php',
+            data: obj
+        }).done(function(result) {
+            
 
-		}).fail(function(a,b,c) {
-			console.log('AJAX Call failed');
-			console.log('tried to send this data:');
-			console.log(obj);
-			console.log('response data:');
-			console.log(a);
-			console.log(b);
-			console.log(c);
-		});
-		
-	},
+        }).fail(function(a,b,c) {
+            console.log('AJAX Call failed');
+            console.log('tried to send this data:');
+            console.log(obj);
+            console.log('response data:');
+            console.log(a);
+            console.log(b);
+            console.log(c);
+        });
+        
+    },
     
     subscribe: function(userData) {
         var self = this;
@@ -174,33 +172,33 @@ var Shop = Backbone.Model.extend({
         });
 
     }
-	
+    
 });
 
 
 
 $(document).ready(function() {
-	
-	
-	window.Einzlstck = {
-		Pages: {},
-		Models: {
-			
-		},
-		Views: {
-			
-		},
-		Deferreds: {
-			products: $.Deferred(),
+    
+    
+    window.Einzlstck = {
+        Pages: {},
+        Models: {
+            
+        },
+        Views: {
+            
+        },
+        Deferreds: {
+            products: $.Deferred(),
             copy: $.Deferred()
-		}
-		
-	};
-	
-	// let's to this
-	Einzlstck.Models.Shop = new Shop();
+        }
+        
+    };
+    
+    // let's to this
+    Einzlstck.Models.Shop = new Shop();
     Einzlstck.Models.User = new User();
-	Einzlstck.Models.Cart = new Basket();
+    Einzlstck.Models.Cart = new Basket();
     
     Einzlstck.Router = new Router(config);
 });
